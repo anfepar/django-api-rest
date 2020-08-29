36feb54c3aec25c0b6552a8d69157ba1fc114888
Andres Felipe Pardo Mesa <fpardo@muvobike.com>
2020-08-29 18:51:45 -0500

    add colors and sizes in product, serialize many to manuy attributes

--- products/models.py
+++ products/models.py
@@ -1,7 +1,25 @@
 from django.db import models
-from django.db import models
 
 # Create your models here.
+class Size(models.Model):
+    name= models.CharField(
+        max_length=2
+    )
+
+    def __str__(self):
+        return self.name
+
+class Color(models.Model):
+    name= models.CharField(
+        max_length=20
+    )
+    ref = models.CharField(
+        max_length = 7
+    )
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     name = models.CharField(
@@ -16,9 +34,14 @@ class Product(models.Model):
     category = models.CharField(
         max_length=50
     )
+
+    sizes = models.ManyToManyField(Size, blank =True)
+    colors = models.ManyToManyField(Color, blank =True)
     description = models.TextField()
     created = models.DateTimeField(auto_now_add = True)
     modified = models.DateTimeField(auto_now= True)
 
     def __str__(self):
         return self.name
+
+
--- products/serializers.py
+++ products/serializers.py
@@ -1,7 +1,19 @@
-from products.models import Product
+from products.models import Product, Color, Size
 from rest_framework import serializers
 
+class ColorSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Color
+        fields = ["name", "ref"]
+
+class SizeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Size
+        fields = ["name"]
+
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    colors = ColorSerializer(read_only=True,many=True)
+    sizes = SizeSerializer(read_only=True, many=True)
     class Meta:
         model = Product
-        fields = ["name","price","picture","description","created","modified", "category"]
+        fields = ["pk", "name","price","picture","description","created","modified", "category",  "sizes", "colors"]
