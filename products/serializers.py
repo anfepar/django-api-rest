78e98142e5c01bb27855aafcd28b2cb5bb7e5ead
Andres Felipe Pardo Mesa <fpardo@muvobike.com>
2020-08-29 19:17:05 -0500

    Add category model

--- products/serializers.py
+++ products/serializers.py
@@ -1,5 +1,11 @@
-from products.models import Product, Color, Size
+from products.models import Product, Color, Size, Category
 from rest_framework import serializers
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model= Category
+        fields = ["name","icon"]
+
 
 class ColorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -14,6 +20,7 @@ class SizeSerializer(serializers.HyperlinkedModelSerializer):
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     colors = ColorSerializer(read_only=True,many=True)
     sizes = SizeSerializer(read_only=True, many=True)
+    category = CategorySerializer(read_only= True)
     class Meta:
         model = Product
         fields = ["pk", "name","price","picture","description","created","modified", "category",  "sizes", "colors"]
